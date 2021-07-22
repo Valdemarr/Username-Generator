3320865bc8ae10d0515c64e5ffb9728290c1df65
Valdemar Skjøth Skjøth <valdemarsthoisen@gmail.com>
2021-07-22 20:50:35 +0000

    Broken option of adding numbers fixed typo added idea

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import random
 
 adjectives = ["Blue", "Human", "Salty", "Big", "Sour", "Round", "Ugly", "Pretty", "Odd", "Long", "Fat"]
-nouns = ["Human", "Ball", "Sky", "Remote", "Child", "Chips", "Youngster", "Bevarage", "Sidewalk", "Meat" , "Yeeter"]
+nouns = ["Human", "Ball", "Sky", "Remote", "Child", "Chips", "Youngster", "Beverage", "Sidewalk", "Meat" , "Yeeter"]
 
-two_r_adj = random.sample(adjectives, 2)
+r_adj_x2 = random.sample(adjectives, 2)
 
 r_nou = random.choice(nouns)
 
-print(f" Your name is: {two_r_adj[0]} {two_r_adj[1]} {r_nou}")
+r_num = random.choice(range(10, 101))
+
+print("Do you want numbers in your name? Type Y / N:")
+reply = input()
+
+if reply == "Y" or "y":
+  print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}{r_num}")
+elif reply == "N" or "n":  
+  print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
+else:
+  print (f"You should really learn to read instructions... But here's your username without numbers: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
+
+#Print are you ready for a name? we have len(adjectives) x len(nouns) unique combinations
