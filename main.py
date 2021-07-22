819074b1b9d251c84c05baca4fec52540604897d
Valdemar Skjøth Skjøth <valdemarsthoisen@gmail.com>
2021-07-22 21:21:01 +0000

    Fixed number option

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ r_num = random.choice(range(10, 101))
 print("Do you want numbers in your name? Type Y / N:")
 reply = input()
 
-if reply == "Y" or "y":
+if reply == "Y" or reply == "y":
   print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}{r_num}")
-elif reply == "N" or "n":  
+elif reply == "N" or reply == "n":  
   print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
 else:
   print (f"You should really learn to read instructions... But here's your username without numbers: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
