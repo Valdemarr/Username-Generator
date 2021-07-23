d92ecba145f0693e517710affefa3c609d892cca
Valdemar Skjøth Skjøth <valdemarsthoisen@gmail.com>
2021-07-23 10:39:12 +0000

    Easier toggling of number option than commenting

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ r_nou = random.choice(nouns)
 r_num = random.choice(range(10, 101))
 
 #Number option
-#print("Do you want numbers in your name? Type Y / N:")
-#reply = input()
-
-#Output
-#if reply == "Y" or reply == "y":
-#  print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}{r_num}")
-#elif reply == "N" or reply == "n":  
-#  print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
-#else:
-#  print (f"You should really learn to read instructions... But here's your username without numbers: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
-
-#REMOVE IF ENABLING NUMBER OPTION
-print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
+use_numbers = False
+if use_numbers is False:
+  print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
+else:
+  print("Do you want numbers in your name? Type Y / N:")
+  reply = input()
+  #Output
+  if reply == "Y" or reply == "y":
+    print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}{r_num}")
+  elif reply == "N" or reply == "n":  
+    print(f" Your name is: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
+  else:
+    print (f"You should really learn to read instructions... But here's your username without numbers: {r_adj_x2[0]}{r_adj_x2[1]}{r_nou}")
 
 
 #Users can cumulatively add new words and expand the database
